e14b90ac65c4e3f63fab216f76a148c7b04b0d61
jakestrouse00 <jakestrouse00@gmail.com>
2019-10-01 20:33:01 -0400

    Updated version

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
+import setuptools
 
-setup(name='pastebot',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='mypastebot',
       description='A Python solution to creating pastes as a guest, listing new public pastes and more',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       version='0.1',
-      url='https://github.com/massenz/filecrypt',
-      author='M. Massenzio',
-      author_email='marco@alertavert.com',
+      url='https://github.com/jakestrouse00/pastebot',
+      author='Jake Strouse',
+      author_email='jakestrouse00@gmail.com',
       license='MIT',
-      packages=['crytto'],
-      nstall_requires=['PyYAML>=3.11', 'sh>=1.11'])
+      packages=setuptools.find_packages(),
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      install_requires=['requests==2.22.0'],
+      python_requires='>=3.6')
